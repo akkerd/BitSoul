9565b20e33b1250e134dae22e914896b45bdf58e
Kasper Juul <kasjuul@gmail.com>
2017-12-03 20:47:35 +0100

    PlayerControl
    Implemented inheritence functionality for the PlayerController class

--- Assets/GeometryPuns/Scripts/PlayerController.cs
+++ Assets/GeometryPuns/Scripts/PlayerController.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float moveSpeed = 7.0f;
 	public float jumpSpeed = 15.0f;
+	public float wallJumpSpeed = 20.0f;
 
 	public Transform groundCheck;
 	public float groundCheckRadius;
+	public Transform rightCheck;
+	public Transform leftCheck;
+	private float wallCheckRadius = 0.4f;
 	public LayerMask whatIsGround;
-	private bool grounded;
+	public LayerMask whatIsWall;
+	protected bool grounded;
+	protected bool Lwalled;
+	protected bool Rwalled;
     private bool faceRight;
 
-	private bool doubleJumped;
+	protected bool doubleJumped;
+	private bool wallJumped = false;
 
-	private Rigidbody2D player_rb;
+	protected Rigidbody2D player_rb;
 
 
-	// Use this for initialization
+	public virtual void Control(){
+		// This can be overwritten by inherited classes
+		// for special abillities
+	}
+		
 	void Start () {
 		player_rb = GetComponent<Rigidbody2D>();
         faceRight = true;
@@ -25,33 +37,38 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate(){
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+		Rwalled = Physics2D.OverlapCircle(rightCheck.position, wallCheckRadius, whatIsWall);
+		Lwalled = Physics2D.OverlapCircle(leftCheck.position, wallCheckRadius, whatIsWall);
 	}
 
 	void Update() {
-		if(grounded){
-			doubleJumped = false;
-		}
-		if(Input.GetKeyDown(KeyCode.Space) && grounded){
-			player_rb.velocity = new Vector2(0.0f, jumpSpeed);
-		}
-		if(Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded){
-			player_rb.velocity = new Vector2(0.0f, jumpSpeed);
-			doubleJumped = true;
-		}
-		if(Input.GetKey(KeyCode.RightArrow)){
-			player_rb.velocity = new Vector2(moveSpeed, player_rb.velocity.y);
-            if (!faceRight)
-                faceRight = true;
-		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			player_rb.velocity = new Vector2(-moveSpeed, player_rb.velocity.y);
-            if (faceRight)
-                faceRight = false;
-        }
+
+		CheckJump();
+		Control();
+		CheckMove();
 	}
 
     public bool isFacingRight()
     {
         return faceRight;
     }
+
+	protected void CheckJump (){
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
+			player_rb.velocity = new Vector2(0.0f, jumpSpeed);
+		}
+	}
+
+	protected void CheckMove(){
+		if(Input.GetKey(KeyCode.RightArrow)){
+			player_rb.velocity = new Vector2(moveSpeed, player_rb.velocity.y);
+			if (!faceRight)
+				faceRight = true;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			player_rb.velocity = new Vector2(-moveSpeed, player_rb.velocity.y);
+			if (faceRight)
+				faceRight = false;
+		}
+	}
 }
