a2a651fbe68cecf47819bf71ad821b935e2b29ac
mloppnow <malloppnow@gmail.com>
2017-12-16 11:29:12 +0100

    Parallax background added

--- Assets/GeometryPuns/Scripts/ScrollingBackgroundLeftRight.cs
+++ Assets/GeometryPuns/Scripts/ScrollingBackgroundLeftRight.cs
@@ -2,54 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollingBackground : MonoBehaviour {
+public class ScrollingBackgroundLeftRight : MonoBehaviour
+{
     public float backgroundSize;
-    public float paralaxSpeed;
+    public float parallaxSpeed;
 
     private float lastCameraX;
     private Transform cameraTransform;
-    public Transform[] layers;
+    private Transform[] layers;
     private float viewZone = 10;
-    public int leftIndex;
-    public int rightIndex;
-	// Use this for initialization
-	void Start () {
+    private int leftIndex;
+    private int rightIndex;
+    // Use this for initialization
+    void Start()
+    {
         cameraTransform = Camera.main.transform;
         lastCameraX = cameraTransform.position.x;
         layers = new Transform[transform.childCount];
-        for(int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             layers[i] = transform.GetChild(i);
         }
 
         leftIndex = 0;
         rightIndex = layers.Length - 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float deltaX = cameraTransform.position.x - lastCameraX;
-        transform.position += Vector3.right * (deltaX * paralaxSpeed);
+        transform.position += Vector3.right * (deltaX * parallaxSpeed);
         lastCameraX = cameraTransform.position.x;
 
-        if(cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+        if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
         {
             ScrollLeft();
         }
 
-        if(cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+        if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
         {
             ScrollRight();
         }
-	}
-
+    }
+    
     private void ScrollLeft()
     {
         int lastRight = rightIndex;
         layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
         leftIndex = rightIndex;
         rightIndex--;
-        if(rightIndex < 0)
+        if (rightIndex < 0)
         {
             rightIndex = layers.Length - 1;
         }
@@ -59,7 +62,7 @@ public class ScrollingBackground : MonoBehaviour {
     {
         int lastLeft = leftIndex;
         layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
-        rightIndex = leftIndex; 
+        rightIndex = leftIndex;
         leftIndex++;
         if (leftIndex == layers.Length)
         {
